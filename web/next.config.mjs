ce1a658b75255be8970e0fbb37e8f29922f7e6b9
Heng_Xin_666 <282000500@qq.com>
2025-08-21 23:48:01 +0800

    配置前端, 指定前缀

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,19 +1,17 @@
+// next.config.mjs
 const isProd = process.env.NODE_ENV === "production";
-
 const internalHost = process.env.TAURI_DEV_HOST || "localhost";
+const repoName = "BiLiBiLi_DanMu_Crawling"; // GitHub 仓库名
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 确保 Next.js 使用 SSG 而不是 SSR
-  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-  output: "export",
-  // 注意：在 SSG 模式下使用 Next.js 的 Image 组件需要此功能。
-  // 请参阅 https://nextjs.org/docs/messages/export-image-api 了解不同的解决方法。
+  output: "export",             // 确保静态导出
+  trailingSlash: true,          // 生成 /about/index.html 等, 避免 404
   images: {
-    unoptimized: true,
+    unoptimized: true,          // 静态导出不优化图片
   },
-  // 配置 assetPrefix，否则服务器无法正确解析您的资产。
-  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
+  basePath: isProd ? `/${repoName}` : "",           // GitHub Pages 仓库路径
+  assetPrefix: isProd ? `/${repoName}/` : `http://${internalHost}:3000`, // 静态资源前缀
 };
 
 export default nextConfig;
